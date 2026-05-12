98deccb16c65700dbaf64af53a101fe8c4869d12
VijayKharkar <vijaykharkar26@gmail.com>
2026-05-12 13:43:56 +0530

    production  wise vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000
+  },
+
+  define: {
+    __APP_ENV__: JSON.stringify(process.env.NODE_ENV),
+  }
 })
